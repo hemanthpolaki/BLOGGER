9b03e858c48fd5f11df123f4584dbb5518f15756
hemanthpolaki <hemanthpolaki7@gmail.com>
2020-10-22 14:50:25 +0530

    xx

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
